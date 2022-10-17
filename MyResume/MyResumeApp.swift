4e7de036b4ad2ae1503eb3d108d26163d91fe5c3
cleopatra <kelly912718@gmail.com>
2022-10-17 20:32:08 +0800

    Initial Commit

---
+++ MyResume/MyResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyResumeApp.swift
+//  MyResume
+//
+//  Created by 黃郁雯 on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct MyResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
